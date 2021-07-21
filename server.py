f62cedb7a8105bf55ea9986d4a836e28d0b1586e
Jian-G <541832360@qq.com>
2021-07-21 12:04:37 +0800

    add model_load module

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import glob
 import json
 import struct
 from threading import Thread
-
+from load_model import edge_load_model
 model_dict = []
 param_dict = []
 tensor_dict = []
@@ -21,12 +21,12 @@ def send_loop(type):
                   format(core.CLOUD_HOST,core.CLOUD_SENTTO_EDGE,addr[0],addr[1]))
             while True:
                 # 发送pdmodel文件
-                for filename in glob.glob(r'data/send/model/*.pdmodel'):
+                for filename in glob.glob(r'data/send/model/client_infer_*.pdmodel'):
                     if(filename not in model_dict):
                         model_dict.append(filename)
                         send_file(conn, filename)
                 # 发送pdiparams文件
-                for filename in glob.glob(r'data/send/model/*.pdparams'):
+                for filename in glob.glob(r'data/send/model/client_infer_*.pdiparams'):
                     if(filename not in param_dict):
                         param_dict.append(filename)
                         send_file(conn, filename)
@@ -39,11 +39,16 @@ def send_loop(type):
             conn, addr = server.accept()
             print("Edge Server(I) {} : {} has connected to Cloud client(others) {} : {}".
                   format(core.EDGE_HOST,core.EDGE_SENDTO_CLOUD,addr[0],addr[1]))
+            index = 0
             while True:
-                for filename in glob.glob(r'data/send/tensor/*.txt'):
-                    if(filename not in tensor_dict):
-                        tensor_dict.append(filename)
-                        send_file(conn, filename)
+                tensor = edge_load_model(path_prefix="./data/receive/model/client_infer_resnet18_cifar10")
+                send_tensor(conn, tensor, index)
+                index += 1
+                time.sleep(10)
+                # for filename in glob.glob(r'data/send/tensor/*.txt'):
+                #     if(filename not in tensor_dict):
+                #         tensor_dict.append(filename)
+                #         send_file(conn, filename)
 
 
 def send_file(conn, filename):
@@ -63,6 +68,22 @@ def send_file(conn, filename):
         data = f.read()
         conn.sendall(data)
     print("\nFile {} ({} MB) send finish.".format(filename, round(filesize/1000/1000,2)))
+
+def send_tensor(conn, tensor, name):
+    tensorsize = os.path.getsize(tensor)
+    dict = {
+        'filename': name,
+        'filesize': tensorsize,
+    }
+    head_info = json.dumps(dict)
+    head_info_len = struct.pack('i', len(head_info))
+    # 发送头部长度
+    conn.send(head_info_len)
+    # 发送头部信息
+    conn.send(head_info.encode('utf-8'))
+    conn.send(tensor)
+    print("\nFile {} ({} MB) send finish.".format(name, round(tensor/1000/1000,2)))
+
 
 if __name__ == '__main__':
     edge_server = Thread(target=send_loop, args=("cloud", ))
